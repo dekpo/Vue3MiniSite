2150d6acd1504511c42695bcbb35d9a5a2977a37
Dekpo Yologaza <dekpo@me.com>
2020-12-17 15:22:20 +0100

    Added vue.config.js to specify build options

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
-exports = {
+const path = require('path');
+module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-      ? '/Vue3MiniSite/'
-      : '/'
+    ? '/Vue3MiniSite/'
+    : '/',
+    outputDir: path.resolve(__dirname,'./docs'),
   }
