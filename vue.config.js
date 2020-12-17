ec51402be909087fdfd4cdc7b8dc8730e3603dac
Dekpo Yologaza <dekpo@me.com>
2020-12-17 15:52:24 +0100

    Made a new build...

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-    ? '/Vue3MiniSite/'
+    ? './'
     : '/',
     outputDir: path.resolve(__dirname,'./docs'),
   }
